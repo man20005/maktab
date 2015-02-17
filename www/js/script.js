12991f8ccf8b3aba859fdce07060b2fe5e798380
samirhassen <hassen.samir@gmail.com>
2015-02-17 15:07:22 +0300

    Form klar mer eller mindre
    post array tom

---
+++ www/js/script.js
@@ -0,0 +1,27 @@
+angular.module("formApp", []).controller("formController", function($scope, $http) {
+
+// create a blank object to hold our form information
+// $scope will allow this to pass between controller and view
+$scope.formData = {};
+
+// process the form
+$scope.processForm = function(formData) {
+	$http({
+		method : 'POST',
+		url : 'http://www.wasmiah.com/insComp.php',
+		data : $scope.formData,
+		headers : { 'Content-Type': 'application/x-www-form-urlencoded' } // set the headers so angular passing info as form data (not request payload)
+	})
+		.success(function(data) {
+		console.log(data);
+	if (!data.success) {
+		// if not successful, bind errors to error variables
+		$scope.errorName = data.errors.name;
+		//TODO.. the others
+	} else {
+		// if successful, bind success message to message
+		$scope.message = data.message;
+			}
+		});
+	};
+});
